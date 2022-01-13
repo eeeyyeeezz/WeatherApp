f01f3b98489ed651943c67e12d675cd8585fef7f
eeeyyeeezz <eximmlich@gmail.com>
2022-01-13 15:51:10 +0300

    all views + logic done only design stuff left

---
+++ WeatherApp/View/WeatherCollectionView/SetInterfaceWeatherCollectionViewCell.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+extension WeatherCollectionViewCell {
+	
+	func setInterface(){
+		getForecastData()
+		completionHandler = { newStruct in
+			DispatchQueue.main.async {
+				self.forecstStruct = newStruct
+				guard let idCell = self.idCell else { print("IdCell Error") ; return }
+				let temp = "\(newStruct.list[idCell * 4].main.temp) °C"
+				let dtTxt = newStruct.list[idCell * 4].dtTxt
+				let time = dtTxt.dropFirst(11).dropLast(3)
+				let date = "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 5)])" + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 6)])" + "." + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 8)])" + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 9)])"
+				self.nameLabel.text = temp + "\n" + date + "\n" + time
+				self.setImage()
+			}
+		}
+		nameLabel.text = cityString
+	}
+	
+	func setImage(){
+		let clearSky = ["Clear", "Clear sky"]
+		let clouds = ["Clouds", "Few clouds", "Scattered clouds", "Broken clouds"]
+		let rains = ["Rain", "Shower Rain"]
+		let thunderstorm = "Thunderstorm"
+		let snow = "Snow"
+		let mist = "Mist"
+		
+		
+		guard let idCell = idCell else { print("Id Cell error in setImage") ; return }
+		guard let forecastStruct = self.forecstStruct else { print("SetForecastError Error") ; return }
+		guard let weather = forecastStruct.list[idCell * 4].weather.first?.main else { print("SetForecastInterface Error") ; return }
+		
+		if clearSky.contains(weather) {
+			
+			weatherImage.image = UIImage(named: "sun")
+		} else if weather == snow {
+			
+			weatherImage.image = UIImage(named: "snow")
+		} else if rains.contains(weather){
+			
+			weatherImage.image = UIImage(named: "rain")
+		} else if clouds.contains(weather){
+			
+			weatherImage.image = UIImage(named: "cloud")
+		} else if weather == thunderstorm {
+			
+			weatherImage.image = UIImage(named: "thunderstorm")
+		} else if weather == mist {
+			
+			weatherImage.image = UIImage(named: "mist")
+		}
+		
+	}
+	
+}
--- WeatherApp/View/WeatherCollectionView/WeatherCollectionViewCell.swift
+++ WeatherApp/View/WeatherCollectionView/WeatherCollectionViewCell.swift
@@ -25,7 +25,8 @@ class WeatherCollectionViewCell: UICollectionViewCell {
 	
 	let weatherImage: UIImageView = {
 		let image = UIImageView()
-		image.translatesAutoresizingMaskIntoConstraints = false
+		image.frame = CGRect(x: 5, y: 50, width: 70, height: 80)
+//		image.translatesAutoresizingMaskIntoConstraints = false
 		return image
 	}()
 	
@@ -46,72 +47,18 @@ class WeatherCollectionViewCell: UICollectionViewCell {
 			nameLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 5)
 		])
 		
-//		addSubview(weatherImage)
+		addSubview(weatherImage)
 //		NSLayoutConstraint.activate([
 //			weatherImage.leadingAnchor.constraint(equalTo: leadingAnchor),
 //			weatherImage.trailingAnchor.constraint(equalTo: trailingAnchor),
 //			weatherImage.topAnchor.constraint(equalTo: topAnchor),
-//			weatherImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 20)
+//			weatherImage.bottomAnchor.constraint(equalTo: bottomAnchor, constant: 20),
 //		])
 		
 	}
 	
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
-	}
-	
-}
-
-extension WeatherCollectionViewCell {
-	
-	func setInterface(){
-		getForecastData()
-		completionHandler = { newStruct in
-			DispatchQueue.main.async {
-				self.forecstStruct = newStruct
-				guard let idCell = self.idCell else { print("IdCell Error") ; return }
-				print("ID CELL IS " + "\(idCell)")
-				let temp = "\(newStruct.list[idCell + 4].main.temp) °C"
-				let dtTxt = newStruct.list[idCell + 4].dtTxt
-				let date = "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 5)])" + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 6)])" + "." + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 8)])" + "\(dtTxt[dtTxt.index(dtTxt.startIndex, offsetBy: 9)])"
-				self.nameLabel.text = temp + "\n" + date
-				self.setImage()
-			}
-		}
-		nameLabel.text = cityString
-	}
-	
-	func setImage(){
-		let clearSky = ["Clear", "Clear sky"]
-		let clouds = ["Clouds", "Few clouds", "Scattered clouds", "Broken clouds"]
-		let rains = ["Rain", "Shower Rain"]
-		let thunderstorm = "Thunderstorm"
-		let snow = "Snow"
-		let mist = "Mist"
-		
-		guard let forecastStruct = self.forecstStruct else { print("SetForecastError Error") ; return }
-		guard let weather = forecastStruct.list.first?.weather.first?.main else { print("SetForecastInterface Error") ; return }
-		
-		if clearSky.contains(weather) {
-//			nameLabel.text = "Clear Sky"
-			weatherImage.image = UIImage(named: "sun")
-		} else if weather == snow {
-//			nameLabel.text = "Snow"
-			weatherImage.image = UIImage(named: "snow")
-		} else if rains.contains(weather){
-//			nameLabel.text = "Rain"
-			weatherImage.image = UIImage(named: "rain")
-		} else if clouds.contains(weather){
-//			nameLabel.text = "Cloud"
-			weatherImage.image = UIImage(named: "cloud")
-		} else if weather == thunderstorm {
-//			nameLabel.text = "Thunderstorm"
-			weatherImage.image = UIImage(named: "thunderstorm")
-		} else if weather == mist {
-//			nameLabel.text = "Mist"
-			weatherImage.image = UIImage(named: "mist")
-		}
-		
 	}
 	
 }
